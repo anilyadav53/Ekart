e1ae40d3dba3fbd60a49f6409d11bb311759d844
anilyadav53 <64617163+anilyadav53@users.noreply.github.com>
2025-02-21 14:19:29 +0530

    Update CustomErrorController.java

--- src/main/java/com/reljicd/controller/CustomErrorController.java
+++ src/main/java/com/reljicd/controller/CustomErrorController.java
@@ -1,6 +1,6 @@
 package com.reljicd.controller;
 
-import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +19,5 @@ public class CustomErrorController implements ErrorController {
     @GetMapping("/403")
     public ModelAndView error403() {
         return new ModelAndView("/403");
-    }
-
-    @Override
-    public String getErrorPath() {
-        return PATH;
     }
 }
